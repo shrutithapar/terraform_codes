520b4e9a680502324cac1f2beedf7de4f2caa76f
EC2 Default User <ec2-user@ip-172-31-26-51.us-east-2.compute.internal>
2020-09-18 14:52:36 +0000

    first commit

--- ec2/output.tf
+++ ec2/output.tf
@@ -1,4 +1,10 @@
 output "instances" {
    description = "AWS Instances created in lab"
-   value = aws_instance.myec2.*.public_dns
+   value = aws_instance.myec2.public_dns
 }
+
+output "public_ip" {
+   description = "AWS Instances created in lab"
+   value = aws_instance.myec2.public_ip
+}
+
